b1d173537e7a069709034544720c5b7e0e56aeff
Bona Akubue <bonaakubue@gmail.com>
2024-02-09 06:01:38 -0800

    syntax

---
+++ ch01/ex.py
@@ -0,0 +1,21 @@
+#writing a single line comment
+#my first program
+
+"""
+This is a multi-line comment
+"""
+
+#indentations and spaces
+for num in [1,2,3,4,5]:
+    #print num
+    print(num)
+
+#Assigning variables
+num =  5
+
+result = num + 4
+print(result)
+
+#using descriptive variable names
+age = 25 
+a = 25
