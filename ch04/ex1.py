c4779c836b17ea767df40822c38308e01f800c3d
Bona Akubue <bonaakubue@gmail.com>
2024-02-11 02:25:02 -0800

    conditional statements

---
+++ ch04/ex1.py
@@ -0,0 +1,42 @@
+
+#if statement
+num = 4
+if num % 2 == 0:
+    print('an even number')
+
+#else statements
+if num % 2 == 0:
+    print('an even number')
+else:
+    print('an odd number')
+
+
+#else statements
+if num % 2 == 0:
+    print('an even number')
+else:
+    print('an odd number')
+
+
+#elif statement
+if num % 2 == 0:
+    print('an even number')
+elif num % 2 != 0:
+    print('an odd number')
+
+#multiple elif statement
+if num % 2 == 0:
+    print('an even number')
+elif num % 3 == 0:
+    print('divisible by 3')
+elif num % 4 != 0:
+    print('divisible by 4')
+
+#nested if statements
+if num > 0:
+    if num % 2 == 0:
+        print('even number')
+
+
+
+
