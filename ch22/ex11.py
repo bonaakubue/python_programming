7c0599c376b4a43f88307e51de18dd701ffe3e66
Bona Akubue <bonaakubue@gmail.com>
2024-02-12 07:26:11 -0800

    descriptors and decorators

--- ch22/ex11.py
+++ ch22/ex11.py
@@ -1,20 +1,20 @@
 #validating names using set
 
-class Movement:
-    def __get__(self, instance, owner=None):
-        moves = {'Bird':'flies', 'Fish':'swims', 'Dog': 'walks or runs'}
-        return moves[instance.name]
+# class Movement:
+#     def __get__(self, instance, owner=None):
+#         moves = {'Bird':'flies', 'Fish':'swims', 'Dog': 'walks or runs'}
+#         return moves[instance.name]
 
-    def __set__(self, instance, value):
-        if instance.name not in moves:
-            raise ValueError('Incorrect name of animal')
-        instance.name = value
+#     def __set__(self, instance, value):
+#         if instance.name not in moves:
+#             raise ValueError('Incorrect name of animal')
+#         instance.name = value
 
-class Animal:
-    movement = Movement()
+# class Animal:
+#     movement = Movement()
 
-    def __init__(self, name):
-        self.name = name
+#     def __init__(self, name):
+#         self.name = name
 
-animal = Animal('Bird')
-print(animal.movement)
+# animal = Animal('Bird')
+# print(animal.movement)
